4a5bc018e263264a39c45c7f07b57fb8779bca2b
Michal Schwarz <michal.schwarz@gmail.com>
2024-04-29 16:57:21 +0200

    refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -17,27 +17,43 @@
 #include "main.h"
 // clang-format on
 
+#define SECONDS_TO_MILLIS 1000
+#define MILLIS 1
+
+#define TIMEZONE "CET-1CEST,M3.5.0,M10.5.0/3"  // Europe/Prague
+#define NTP_SERVER "cz.pool.ntp.org"
+
 // hardware configuration
 #define HARDWARE_TYPE MD_MAX72XX::DR0CR0RR0_HW
-#define MAX_DEVICES 2  // FIXME tmp, should be 2
+#define MAX_DEVICES 2
 #define DATA_PIN 9
 #define CS_PIN 10
 #define CLK_PIN 11
+// SPI hardware interface
+// MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+// Specific SPI hardware interface
+// MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, SPI1, CS_PIN, MAX_DEVICES);
+// Arbitrary pins (bit banged SPI, no problem with low speed devices like MAX72xx)
+MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
 
-#define SECONDS_TO_MILLIS 1000
-#define MILLIS 1
+// Internal LED matrix:
+//
+// XXX XXX X XXX XXX   XXX XXX
+// X X X X X X X X X X X X X X
+// XXX XXX   XXX XXX   XXX XXX
+// X X X X X X X X X X X X X X
+// XXX XXX X XXX XXX   XXX XXX
+
+// how the visible screen is mapped to the physical screen
+#define CLOCK_ROWS 5
+#define CLOCK_COLUMNS 20
+#define PHYSICAL_DIGIT_PINS 16
+#define PHYSICAL_SEGMENT_PINS 6
 
 WiFiManager wifiManager;
 WiFiClient wifiClient;
 time_t lastTime;
 Timemark blinkingDotsIndicator(500 * MILLIS);
-
-// SPI hardware interface
-// MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
-// Specific SPI hardware interface
-// MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, SPI1, CS_PIN, MAX_DEVICES);
-// Arbitrary pins (bit banged SPI)
-MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
 
 void wdtInit() {
 #ifdef USE_WDT
@@ -139,75 +155,24 @@ void test_blinking()
   Serial.println("Stopped blinking");
 };
 
-void test_on_off() {
-  for (int i = 0; i < 100; i++) {
-    Serial.println("ON");
-    for (int col = 0; col < COL_SIZE; col++) {
-      mx.setColumn(0, col, 0b11111111);
-    }
-    delay(500);
-    Serial.println("OFF");
-    for (int col = 0; col < COL_SIZE; col++) {
-      mx.setColumn(0, col, 0b00000000);
-    }
-    delay(500);
-  }
-};
-
-void test_simple_on() {
-  while (1) {
-    mx.setColumn(0, 0, 0b11111111);
-    mx.setColumn(0, 1, 0b11111111);
-    mx.setColumn(0, 2, 0b11111111);
-    mx.setColumn(0, 3, 0b11111111);
-    mx.setColumn(0, 4, 0b11111111);
-    mx.setColumn(0, 5, 0b11111111);
-    mx.setColumn(0, 6, 0b11111111);
-    mx.setColumn(0, 7, 0b11111111);
-    delay(100000);
-    // mx.setColumn(0, 0, 0x00);
-    // delay(1000);
-  };
-};
-
-/*
-
-Physical LED matrix:
-
-XXX XXX X XXX XXX   XXX XXX
-X X X X X X X X X X X X X X
-XXX XXX   XXX XXX   XXX XXX
-X X X X X X X X X X X X X X
-XXX XXX X XXX XXX   XXX XXX
-
-Controlling pins on the LED matrix:
-C0-C15 = GND, mapped to the MAX7219 digits
-R0-R6  = Vcc, mapped to the MAX7219 segments
-
-*/
-
-#define DISPLAY_ROWS 5
-#define DISPLAY_COLS 20
-
-#define PHYSICAL_DIGIT_PINS 16
-#define PHYSICAL_SEGMENT_PINS 7
-
 // Mapping of define matrix of 8 rows and 16 columns
-uint8_t map_virt_to_phys_cords[DISPLAY_ROWS][DISPLAY_COLS] = {
-    {0x00, 0x11, 0x21, 0x31, 0x10, 0x40, 0x50, 0x60, 0x71, 0x81, 0x91, 0x70, 0xa0, 0xff, 0xb0, 0xc1, 0xd1, 0xe1, 0xc0, 0xf0},
-    {0x02, 0xff, 0x22, 0x32, 0xff, 0x42, 0x52, 0x62, 0xff, 0x82, 0x92, 0xff, 0xa2, 0x51, 0xb2, 0xff, 0xd2, 0xe2, 0xff, 0xf2},
-    {0x03, 0x13, 0x23, 0x33, 0x12, 0x43, 0xff, 0x63, 0x73, 0x83, 0x93, 0x72, 0xa3, 0xff, 0xb3, 0xc3, 0xd3, 0xe3, 0xc2, 0xf3},
-    {0x04, 0xff, 0x24, 0x34, 0xff, 0x44, 0x54, 0x64, 0xff, 0x84, 0x94, 0xff, 0xa4, 0x53, 0xb4, 0xff, 0xd4, 0xe4, 0xff, 0xf4},
-    {0x06, 0x16, 0x26, 0x36, 0x14, 0x46, 0x56, 0x66, 0x76, 0x86, 0x96, 0x74, 0xa6, 0xff, 0xb6, 0xc6, 0xd6, 0xe6, 0xc4, 0xf6},
+// clang-format off
+int map_visible_to_internal_cords[CLOCK_ROWS][CLOCK_COLUMNS] = {
+    {0x00, 0x11, 0x21, 0x31, 0x10, 0x40, 0x50, 0x60, 0x71, 0x81, 0x91, 0x70, 0xa0,   -1, 0xb0, 0xc1, 0xd1, 0xe1, 0xc0, 0xf0},
+    {0x02, -1,   0x22, 0x32,   -1, 0x42, 0x52, 0x62,   -1, 0x82, 0x92,   -1, 0xa2, 0x51, 0xb2,   -1, 0xd2, 0xe2,   -1, 0xf2},
+    {0x03, 0x13, 0x23, 0x33, 0x12, 0x43,   -1, 0x63, 0x73, 0x83, 0x93, 0x72, 0xa3,   -1, 0xb3, 0xc3, 0xd3, 0xe3, 0xc2, 0xf3},
+    {0x04, -1,   0x24, 0x34,   -1, 0x44, 0x54, 0x64,   -1, 0x84, 0x94,   -1, 0xa4, 0x53, 0xb4,   -1, 0xd4, 0xe4,   -1, 0xf4},
+    {0x05, 0x15, 0x25, 0x35, 0x14, 0x45, 0x55, 0x65, 0x75, 0x85, 0x95, 0x74, 0xa5,   -1, 0xb5, 0xc5, 0xd5, 0xe5, 0xc4, 0xf5},
 };
+// clang-format on
 
-uint8_t virtual_screen[DISPLAY_ROWS][DISPLAY_COLS];
-uint8_t physical_screen[PHYSICAL_SEGMENT_PINS][PHYSICAL_DIGIT_PINS];
+uint8_t visible_screen[CLOCK_ROWS][CLOCK_COLUMNS];
+uint8_t internal_screen[PHYSICAL_SEGMENT_PINS][PHYSICAL_DIGIT_PINS];
 
-void debugShowVirtualScreen() {
-  for (int row = 0; row < DISPLAY_ROWS; row++) {
-    for (int col = 0; col < DISPLAY_COLS; col++) {
-      Serial.print(virtual_screen[row][col] ? "▓▓" : "  ");
+void debugShowVisibleScreen() {
+  for (int row = 0; row < CLOCK_ROWS; row++) {
+    for (int col = 0; col < CLOCK_COLUMNS; col++) {
+      Serial.print(visible_screen[row][col] ? "▓▓" : "  ");
       if (col == 2 || col == 5 || col == 6 || col == 9 || col == 12 || col == 13 || col == 16) {
         Serial.print("  ");
       }
@@ -217,11 +182,11 @@ void debugShowVirtualScreen() {
   Serial.println();
 };
 
-void debugShowPhysicalScreen() {
+void debugShowInternalData() {
   for (int segment = 0; segment < PHYSICAL_SEGMENT_PINS; segment++) {
     Serial.printf("Segment (row) %d: ", segment);
     for (int digit = 0; digit < PHYSICAL_DIGIT_PINS; digit++) {
-      if (physical_screen[segment][digit]) {
+      if (internal_screen[segment][digit]) {
         Serial.printf("%1x", digit);
       } else {
         Serial.print(" ");
@@ -232,93 +197,103 @@ void debugShowPhysicalScreen() {
   Serial.println();
 };
 
-void copyVirtualToPhysical() {
-  for (int row = 0; row < DISPLAY_ROWS; row++) {
-    for (int col = 0; col < DISPLAY_COLS; col++) {
-      uint8_t digit = map_virt_to_phys_cords[row][col] >> 4;
-      uint8_t segment = map_virt_to_phys_cords[row][col] & 0x0F;
-      if (digit < PHYSICAL_DIGIT_PINS && segment < PHYSICAL_SEGMENT_PINS) {
-        physical_screen[segment][digit] = virtual_screen[row][col];
+void updateInternalScreen() {
+  for (int row = 0; row < CLOCK_ROWS; row++) {
+    for (int col = 0; col < CLOCK_COLUMNS; col++) {
+      if (map_visible_to_internal_cords[row][col] == -1) {
+        continue;
       }
+
+      uint8_t digit = map_visible_to_internal_cords[row][col] >> 4;
+      uint8_t segment = map_visible_to_internal_cords[row][col] & 0x0F;
+      if (digit >= PHYSICAL_DIGIT_PINS || segment >= PHYSICAL_SEGMENT_PINS) {
+        continue;
+      };
+
+      internal_screen[segment][digit] = visible_screen[row][col];
     }
   }
 };
 
-void showPhysicalScreen() {
+void sendInternalScreenToDevice() {
   for (int digit = 0; digit < PHYSICAL_DIGIT_PINS; digit++) {
     int mask = 0;
     for (int i = 0; i < PHYSICAL_SEGMENT_PINS; i++) {
       mask <<= 1;
-      mask |= physical_screen[i][digit];
+      mask |= internal_screen[i][digit];
     }
     mx.setColumn(digit, mask);
   }
 };
 
-void showVirtualScreen() {
-  // Serial.println("Showing virtual screen");
-  debugShowVirtualScreen();  // FIXME
+void sendScreenToDevice() {
+  // debugShowVisibleScreen();  // FIXME
 
-  copyVirtualToPhysical();
-
-  debugShowPhysicalScreen();  // FIXME
-  showPhysicalScreen();
+  updateInternalScreen();
+  // debugShowInternalData();  // FIXME
+  sendInternalScreenToDevice();
 };
 
-void clearVirtualScreen() {
-  for (int row = 0; row < DISPLAY_ROWS; row++) {
-    for (int col = 0; col < DISPLAY_COLS; col++) {
-      virtual_screen[row][col] = 0;
+void clearScreen() {
+  for (int row = 0; row < CLOCK_ROWS; row++) {
+    for (int col = 0; col < CLOCK_COLUMNS; col++) {
+      visible_screen[row][col] = 0;
     }
   }
 }
 
-uint8_t digit_pixels[10][DISPLAY_ROWS] = {{// 0
-                                           0b111, 0b101, 0b101, 0b101, 0b111},
-                                          {// 1
-                                           0b001, 0b001, 0b001, 0b001, 0b001},
-                                          {// 2
-                                           0b111, 0b001, 0b111, 0b100, 0b111},
-                                          {// 3
-                                           0b111, 0b001, 0b111, 0b001, 0b111},
-                                          {// 4
-                                           0b101, 0b101, 0b111, 0b001, 0b001},
-                                          {// 5
-                                           0b111, 0b100, 0b111, 0b001, 0b111},
-                                          {// 6
-                                           0b111, 0b100, 0b111, 0b101, 0b111},
-                                          {// 7
-                                           0b111, 0b001, 0b001, 0b001, 0b001},
-                                          {// 8
-                                           0b111, 0b101, 0b111, 0b101, 0b111},
-                                          {// 9
-                                           0b111, 0b101, 0b111, 0b001, 0b111}};
+uint8_t digit_pixels[10][CLOCK_ROWS] = {{// 0
+                                         0b111, 0b101, 0b101, 0b101, 0b111},
+                                        {// 1
+                                         0b001, 0b001, 0b001, 0b001, 0b001},
+                                        {// 2
+                                         0b111, 0b001, 0b111, 0b100, 0b111},
+                                        {// 3
+                                         0b111, 0b001, 0b111, 0b001, 0b111},
+                                        {// 4
+                                         0b101, 0b101, 0b111, 0b001, 0b001},
+                                        {// 5
+                                         0b111, 0b100, 0b111, 0b001, 0b111},
+                                        {// 6
+                                         0b111, 0b100, 0b111, 0b101, 0b111},
+                                        {// 7
+                                         0b111, 0b001, 0b001, 0b001, 0b001},
+                                        {// 8
+                                         0b111, 0b101, 0b111, 0b101, 0b111},
+                                        {// 9
+                                         0b111, 0b101, 0b111, 0b001, 0b111}};
 
-void drawVirtualDigit(int digit, int pos) {
-  for (int row = 0; row < DISPLAY_ROWS; row++) {
+void drawDigit(int digit, int pos) {
+  for (int row = 0; row < CLOCK_ROWS; row++) {
     for (int col = 0; col < 3; col++) {
-      virtual_screen[row][pos + col] = (digit_pixels[digit][row] >> (2 - col)) & 0x01;
+      visible_screen[row][pos + col] = (digit_pixels[digit][row] >> (2 - col)) & 0x01;
     }
+  }
+};
+
+void drawMinusSign(int pos) {
+  for (int col = 0; col < 3; col++) {
+    visible_screen[2][pos + col] = 1;
   }
 };
 
 void drawTime(tm rtcTime) {
   // Serial.printf("Displaying RTC time: %02d:%02d:%02d\n", rtcTime.tm_hour, rtcTime.tm_min, rtcTime.tm_sec);
 
-  drawVirtualDigit(rtcTime.tm_hour / 10, 0);
-  drawVirtualDigit(rtcTime.tm_hour % 10, 3);
-  drawVirtualDigit(rtcTime.tm_min / 10, 7);
-  drawVirtualDigit(rtcTime.tm_min % 10, 10);
-  drawVirtualDigit(rtcTime.tm_sec / 10, 14);
-  drawVirtualDigit(rtcTime.tm_sec % 10, 17);
+  drawDigit(rtcTime.tm_hour / 10, 0);
+  drawDigit(rtcTime.tm_hour % 10, 3);
+  drawDigit(rtcTime.tm_min / 10, 7);
+  drawDigit(rtcTime.tm_min % 10, 10);
+  drawDigit(rtcTime.tm_sec / 10, 14);
+  drawDigit(rtcTime.tm_sec % 10, 17);
 };
 
 void drawDots(bool onOff) {
-  virtual_screen[1][6] = onOff;
-  virtual_screen[3][6] = onOff;
+  visible_screen[1][6] = onOff;
+  visible_screen[3][6] = onOff;
 
-  virtual_screen[1][13] = onOff;
-  virtual_screen[3][13] = onOff;
+  visible_screen[1][13] = onOff;
+  visible_screen[3][13] = onOff;
 }
 
 void setup() {
@@ -354,24 +329,6 @@ void setup() {
   // mx.control(MD_MAX72XX::INTENSITY, MAX_INTENSITY);
   test_blinking();
   Serial.println("Test blinking done");
-  mx.clear();
-  // test_on_off();
-  // test_simple_on();
-  // Serial.println("Test on/off done");
-
-  // // FIXME test
-  // digitalWrite(RGB_BUILTIN, HIGH);  // Turn the RGB LED white
-  // delay(200);
-  // digitalWrite(RGB_BUILTIN, LOW);  // Turn the RGB LED off
-  // delay(200);
-  // neopixelWrite(RGB_BUILTIN, RGB_BRIGHTNESS, 0, 0);  // Red
-  // delay(200);
-  // neopixelWrite(RGB_BUILTIN, 0, RGB_BRIGHTNESS, 0);  // Green
-  // delay(200);
-  // neopixelWrite(RGB_BUILTIN, 0, 0, RGB_BRIGHTNESS);  // Blue
-  // delay(200);
-  // neopixelWrite(RGB_BUILTIN, 0, 0, 0);  // Off / black
-  // delay(200);
 
   lastTime = 0;
   DEBUG_PRINT("Timezone: %s", TIMEZONE);
@@ -379,33 +336,42 @@ void setup() {
   DEBUG_PRINT("Starting NTP sync...");
   configTzTime(TIMEZONE, NTP_SERVER);
   sntp_set_time_sync_notification_cb(cbSyncTime);
+
+  // indicate that time is not synced yet
+  clearScreen();
+  drawMinusSign(0);
+  drawMinusSign(3);
+  drawMinusSign(7);
+  drawMinusSign(10);
+  drawMinusSign(14);
+  drawMinusSign(17);
+  sendScreenToDevice();
 }
 
 void loop() {
-  // Serial.println("Looping...");
   ArduinoOTA.handle();
-  wdtRefresh();
 
   tm rtcTime;
   if (getLocalTime(&rtcTime)) {
+    wdtRefresh();
     time_t curTime = mktime(&rtcTime);
     if (lastTime != curTime) {
       lastTime = curTime;
       blinkingDotsIndicator.start();  // reset
 
-      clearVirtualScreen();
+      clearScreen();
       drawTime(rtcTime);
       drawDots(true);
-      showVirtualScreen();
+      sendScreenToDevice();
     };
   } else {
-    Serial.println("RTC time not available");
     blinkingDotsIndicator.stop();
+    // eventually WDT will reset the device if NTP sync takes too long
   };
 
   if (blinkingDotsIndicator.expired()) {
     drawDots(false);
-    showVirtualScreen();
+    sendScreenToDevice();
   };
 
   delay(5 * MILLIS);

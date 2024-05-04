4d845f6c85dbbdeb44bb42b89485d4f31255f8c2
Michal Schwarz <michal.schwarz@gmail.com>
2024-05-04 16:26:07 +0200

    chore: cleanup

--- include/main.h
+++ include/main.h
@@ -1,30 +1,3 @@
-// external libraries first
-#include <Arduino.h>
-#include <ArduinoOTA.h>
-#include <MD_MAX72xx.h>
-// #include <SPI.h>
-#include <Syslog.h>
-#include <Timemark.h>
-#if defined(ESP32)
-  #include <WiFi.h>
-  #include <esp_sntp.h>
-  #include <esp_task_wdt.h>
-#elif defined(ESP8266)
-  // see https://arduino-esp8266.readthedocs.io/en/latest/ for libraries reference
-  #include <ESP8266WiFi.h>
-#else
-  #error "Unsupported platform"
-#endif
-#include <WiFiManager.h>
-
-// specific headers later
-// clang-format off
-#define LOCAL_DEBUG
-#include "secrets.h"
-#include "local_debug.h"
-// clang-format on
-
-// then everything else
 #define VERSION "20240504.1"
 #define USE_WDT 1
 #define WDT_TIMEOUT 60  // seconds

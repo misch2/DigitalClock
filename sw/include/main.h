c1afa5abdda755fb0ec931095633c4479d383bcb
Michal Schwarz <michal.schwarz@gmail.com>
2024-05-05 11:24:06 +0200

    refactoring: remove explicit WDT calls, rename some methods and vars

--- sw/include/main.h
+++ sw/include/main.h
@@ -1,15 +1,10 @@
-#define VERSION "20240504.2"
-#define USE_WDT 1
-#define WDT_TIMEOUT 60  // seconds
+#define VERSION "20240505.1"
 
 #define MILLIS 1
 #define SECONDS_TO_MILLIS (1000 * MILLIS)
 #define MINUTES_TO_MILLIS (60 * SECONDS_TO_MILLIS)
 #define HOURS_TO_MILLIS (60 * MINUTES_TO_MILLIS)
 
-void wdtInit();
-void wdtRefresh();
-void wdtStop();
 String resetReasonAsString();
 String wakeupReasonAsString();
 void logResetReason();
